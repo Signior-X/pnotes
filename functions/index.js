973157ee5646765edda408881278f42ace648b44
Signior-X <b19188@students.iitmandi.ac.in>
2020-06-15 17:27:05 +0530

    Improved login and made the sign in to server for desktop site

--- functions/index.js
+++ functions/index.js
@@ -23,7 +23,7 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('adjhhikeoapmkhfhionkfn'));  //Secret
 
 app.use(ua.is_mobile());
 app.use(ua.is_tablet());
--- functions/routes/route.js
+++ functions/routes/route.js
@@ -8,11 +8,30 @@ router.get('/', function(req, res, next){
     // console.log(req.is_mobile);
     // console.log(req.is_tablet);
     if (!req.is_mobile && !req.is_tablet) {
-        res.end('Hello World!');
+        // Desktop site
+        if(req.signedCookies.sessionEmail){
+            console.log("User is signed in!");
+            res.render('desktop.ejs');
+        }
+        console.log("Cookie ",req.signedCookies.sessionEmail);
+        res.render('login.ejs');
     } else {
         // This is mobile or tablet site
         res.render('index.ejs');
     }
+});
+
+// Make this set cookie
+router.post('/set', function(req, res, next){
+    res.cookie('sessionEmail', req.body.userEmail, { signed : true });
+    console.log("Cookie set");
+    res.json({success: 1})
+});
+
+router.post('/clear', function(req, res, next){
+    res.clearCookie('sessionEmail');
+    console.log('Cookie cleared');
+    res.json({success: 1});
 });
 
 module.exports = router;
