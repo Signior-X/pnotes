750c326b000e96f47e9f86a5f7d7433898c793c8
Signior-X <b19188@students.iitmandi.ac.in>
2020-06-16 15:19:56 +0530

    Create tab layout

--- public/js/before-body-jquery.js
+++ public/js/before-body-jquery.js
@@ -1,3 +1,14 @@
 $(function() {
     $( "#sidebar" ).resizable();
+
+    $('#preview-button').click(function() {
+        // Make edit button as deactivated
+        makePreviewTabActive();
+    });
+
+    $('#editing-button').click(function() {
+        // Make edit button as deactivated
+        makeEditTabActive();
+    });
+
 });
