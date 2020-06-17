aa3ab097d6ecbeb1bc347e959e31b096fde37cb6
Signior-X <b19188@students.iitmandi.ac.in>
2020-06-17 16:11:29 +0530

    Change scrollbar and activate ctrl+s to save a note

--- public/js/before-body-jquery.js
+++ public/js/before-body-jquery.js
@@ -14,10 +14,23 @@ $(function() {
     $('#dark-switcher').click(function() {
         if($('body').hasClass('dark')) {
             console.log('Already in Dark mode, switching to light');
+            $('#logo-header').html('<img src="pnotes-logo3.png"></img>');
             $('body').removeClass('dark');
         } else{
+            $('#logo-header').html('<img src="pnotes-logo-dark.png"></img>');
             $('body').addClass('dark');
         }
     });
 
+    $('#note-description-editor').bind('keydown', function(event) {
+        if (event.ctrlKey || event.metaKey) {
+            switch (String.fromCharCode(event.which).toLowerCase()) {
+            case 's':
+                // This means save the note now
+                event.preventDefault();
+                saveNote();
+                break;
+            }
+        }
+    });
 });
