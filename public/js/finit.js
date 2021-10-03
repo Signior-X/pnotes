79e5755d60fc7e0e9ab3de38dee48df95cba0726
Priyam <56730716+Signior-X@users.noreply.github.com>
2021-10-03 14:07:13 +0530

    Updated firebase links and added firebase init js (#82)

---
+++ public/js/finit.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+  "projectId": "notesapp-09",
+  "appId": "1:421603176142:web:6b3cc9cbde4be730a7ea9f",
+  "databaseURL": "https://notesapp-09.firebaseio.com",
+  "storageBucket": "notesapp-09.appspot.com",
+  "locationId": "us-central",
+  "apiKey": "AIzaSyCgHI8zrb-qAXcY556RYYTb0SRYRglxopE",
+  "authDomain": "notesapp-09.firebaseapp.com",
+  "messagingSenderId": "421603176142",
+  "measurementId": "G-PKDEQRB23L"
+};
+
+firebase.initializeApp(firebaseConfig);
